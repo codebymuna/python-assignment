126dd98dcf334adbdc470cecfb36817d38cf2895
codebymuna <Muna.Pandey@metropolia.fi>
2026-08-21 20:02:25 +0545

    second commit

---
+++ introo.py
@@ -0,0 +1,8 @@
+print("Hello! Its me Muna. Nice to meet you.")
+#Just a simple intro.
+print("Hello World.")
+name = input("What is your name? ")
+
+print("Hello, " + name + "!")
+
+# print(f"Nice to meet you, {name}!")
